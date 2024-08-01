e2ab4b494e93779686a431da962dd24154d2ae4a
Samar <samarjivraj@icloud.com>
2024-08-01 10:59:04 +0100

    Initial Commit

---
+++ profileAttempt65/profileAttempt65App.swift
@@ -0,0 +1,17 @@
+//
+//  profileAttempt65App.swift
+//  profileAttempt65
+//
+//  Created by Scholar on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct profileAttempt65App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
